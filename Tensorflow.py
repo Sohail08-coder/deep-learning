76aeb0ef422830849b430958eef1b28af835bbab
Sohail <24eg106c62@anurag.edu.in>
2026-07-14 14:41:30 +0530

    Initial commit

---
+++ Tensorflow.py
@@ -0,0 +1,30 @@
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+
+#Data
+x=np.array([[0,0],[0,1],[1,0],[1,1]])
+#print(x)
+y=np.array([[0],[0],[0],[1]])
+print(y)
+
+#Build Architecture
+model=keras.Sequential()
+model.add(layers.Input(shape=(2,)))#Everu neuron recieves 2 inputs
+model.add(layers.Dense(1,activation='sigmoid'))
+model.summary()
+
+#compile
+model.compile( loss='mse' , 
+     optimizer=keras.optimizers.SGD(learning_rate=0.1))
+
+#Train
+model.fit(x,
+          y,
+          epochs=3000)
+    
+    
+    
+    
+    
+print("Hello World")
